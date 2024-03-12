b40a57f9add341100a56c11901d5ad971757e360
Théo Nogueira <theonnogueira@outlook.com>
2024-03-12 12:06:25 -0300

    Projeto finalizado

--- Desktop/Angular 14/memoteca/src/app/app-routing.module.ts
+++ Desktop/Angular 14/memoteca/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   {
@@ -22,6 +23,10 @@ const routes: Routes = [
     path: 'pensamentos/excluir/:id',
     component: ExcluirPensamentoComponent,
   },
+  {
+    path: 'pensamentos/editar/:id',
+    component: EditarPensamentoComponent
+  }
 ];
 
 @NgModule({
--- Desktop/Angular 14/memoteca/src/app/componentes/pensamentos/pensamento.service.ts
+++ Desktop/Angular 14/memoteca/src/app/componentes/pensamentos/pensamento.service.ts
@@ -19,6 +19,11 @@ export class PensamentoService {
     return this.http.post<Pensamento>(this.API, pensamento);
   }
 
+  edit(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API}/${pensamento.id}`;
+    return this.http.put<Pensamento>(url, pensamento);
+  }
+
   delet(id: number): Observable<Pensamento> {
     const url = `${this.API}/${id}`;
     return this.http.delete<Pensamento>(url);
